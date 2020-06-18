d355b06f512a51f280c89b1121516ad83e0d1572
Lim Chee Kin <limcheekin@vobject.com>
2020-06-18 17:48:37 +0800

    first import

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+import '../../../../common/error/failure.dart';
+import '../entities/number_trivia.dart';
+import '../repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  const GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({
+    @required int number,
+  }) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
+}
