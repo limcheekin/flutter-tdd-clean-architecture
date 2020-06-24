d401568dd0133b0473c053fd3e1803fb21034c5e
Lim Chee Kin <limcheekin@vobject.com>
2020-06-24 15:05:48 +0800

    first import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'dependencies.dart' as di;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(MyApp());
 }
 
